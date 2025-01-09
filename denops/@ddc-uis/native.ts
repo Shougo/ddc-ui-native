44683253192c1025b3c1e0f26c83b0a5283c6c6c
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-01-09 12:14:32 +0900

    Update the documentation

--- denops/@ddc-uis/native.ts
+++ denops/@ddc-uis/native.ts
@@ -2,14 +2,14 @@ import {
   type Context,
   type DdcItem,
   type DdcOptions,
-} from "jsr:@shougo/ddc-vim@~7.0.0/types";
-import { BaseUi } from "jsr:@shougo/ddc-vim@~7.0.0/ui";
+} from "jsr:@shougo/ddc-vim@~9.1.0/types";
+import { BaseUi } from "jsr:@shougo/ddc-vim@~9.1.0/ui";
 
 import type { Denops } from "jsr:@denops/core@~7.0.0";
-import * as fn from "jsr:@denops/std@~7.1.1/function";
-import * as autocmd from "jsr:@denops/std@~7.1.1/autocmd";
-import * as op from "jsr:@denops/std@~7.1.1/option";
-import * as vars from "jsr:@denops/std@~7.1.1/variable";
+import * as fn from "jsr:@denops/std@~7.4.0/function";
+import * as autocmd from "jsr:@denops/std@~7.4.0/autocmd";
+import * as op from "jsr:@denops/std@~7.4.0/option";
+import * as vars from "jsr:@denops/std@~7.4.0/variable";
 
 export type Params = {
   insert: boolean;
